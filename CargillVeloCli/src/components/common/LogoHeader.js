50d58ee4d162dd2bbeb381cb82daa10e5c825565
Charan <charan@Charans-MacBook-Pro.local>
2017-07-23 23:10:51 -0500

    deleted package.json file

--- CargillVeloCli/src/components/common/LogoHeader.js
+++ CargillVeloCli/src/components/common/LogoHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const LogoHeader = (props) => {
     const { viewStyle, subHeadTextStyle, phNumberStyle, dataStyle } = styles;
