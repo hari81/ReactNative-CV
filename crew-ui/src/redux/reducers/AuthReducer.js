55a655c723c06e2c1cc56dd4aeadda1b24cae3ee
charan262 <charan.madhi@gmail.com>
2017-10-19 09:14:24 -0500

    conflicts resolved

--- crew-ui/src/redux/reducers/AuthReducer.js
+++ crew-ui/src/redux/reducers/AuthReducer.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import { Alert } from 'react-native';
-=======
->>>>>>> 6f341cd30efbcc92d134b0f538d40a830ba1a58e
 import {
   LOGIN_FAIL,
   LOGIN_USER,
@@ -13,17 +10,10 @@ const INITIAL_STATE = {
   crmSToken: '',
   basicToken: '',
   loading: false,
-<<<<<<< HEAD
-  saveUser: false,
-  logout: false,
-    loginSuccess: false,
-    passwordUpdateMessage: '',
-    DidUpdateFail: ''
-=======
   loginSuccess: false,
   error: false,
-  msg: ''
->>>>>>> 6f341cd30efbcc92d134b0f538d40a830ba1a58e
+  msg: '',
+  passwordUpdateMessage:''
 };
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
@@ -33,13 +23,8 @@ export default (state = INITIAL_STATE, action) => {
       return Object.assign({}, state, { loading: false, error: false, loginSuccess: true, crmSToken: action.crm, basicToken: action.basic });
     case LOGIN_FAIL:
       return Object.assign({}, state, {
-<<<<<<< HEAD
-        error: 'Authentication Failed',
-        message: Alert.alert('Error', 'Invalid Username or Password'),
-=======
         error: true,
         msg: 'Invalid Username or Password',
->>>>>>> 6f341cd30efbcc92d134b0f538d40a830ba1a58e
         loading: false
       });
     case SERVER_NORESPONSE:
@@ -48,19 +33,12 @@ export default (state = INITIAL_STATE, action) => {
         msg: 'Network connection failed',
         loading: false,
       });
-<<<<<<< HEAD
-    case USER_SWITCH_CHANGED:
-      return Object.assign({}, state, { saveUser: action.payload });
-    case LOG_OUT:
-      return Object.assign(({}, state, { logout: action.payload }));
       case 'INVALIDATE_SESSION':
         return Object.assign({}, state, { loginSuccess: false, password: '' });
       case 'PASSWORD_UPDATE_SUCCESS':
           return Object.assign({}, state, { passwordUpdateMessage: Alert.alert('Alert', action.payload) });
       case 'PASSWORD_UPDATE_FAILED':
           return Object.assign({}, state, { passwordUpdateMessage: Alert.alert('Alert', action.payload) });
-=======
->>>>>>> 6f341cd30efbcc92d134b0f538d40a830ba1a58e
     default:
       return state;
   }
