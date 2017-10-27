c30fab19bd6b496bbf16859a021effe583e7b329
Hari <harikrishna_suddapalli@crgl-thirdparty.com>
2017-10-27 15:24:53 -0500

    missed ternary operator change

--- crew-ui/src/components/ExternalTrades/ExternalValues.js
+++ crew-ui/src/components/ExternalTrades/ExternalValues.js
@@ -322,7 +322,7 @@ export default class ExternalValues extends Component {
                                 onChangeText={this.externalTrans.bind(this, 'basis')}
                                 onFocus={this.baisonFocus.bind(this)}
                                 onBlur={() => {
-                                    this.setState({basis: this.state.basis === '' || '-.' ? '' : `$${parseFloat(this.state.basis).toFixed(4)}`});
+                                    this.setState({basis: this.state.basis === '' ? '' : `$${parseFloat(this.state.basis).toFixed(4)}`});
                                 }}
 
                                 keyboardType='numeric'
@@ -344,7 +344,7 @@ export default class ExternalValues extends Component {
                                 onChangeText={this.externalTrans.bind(this, 'adjustments')}
                                 onFocus={this.adjustmentonFocus.bind(this)}
                                 onBlur={() => {
-                                    this.setState({adj: this.state.adj === '' || '-.' ? '' : `$${parseFloat(this.state.adj).toFixed(4)}`});
+                                    this.setState({adj: this.state.adj === '' ? '' : `$${parseFloat(this.state.adj).toFixed(4)}`});
                                 }}
 
                                 keyboardType='numeric'
